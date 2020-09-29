dab2fb49d81edb5532db4727d77ea8538bf93392
Dangermannn <dangermanplyt@gmail.com>
2020-09-29 17:08:21 +0200

    Initial commit for SPA

---
+++ ShoppingPlatform-SPA/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'ShoppingPlatform-SPA';
+  baseUrl = environment.apiUrl;
+  users: any;
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    console.log("URL: " + this.baseUrl);
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get(this.baseUrl + 'users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+}
