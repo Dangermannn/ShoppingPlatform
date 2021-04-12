1c8790a7c1b40c8a7fb8054387455ded828be922
Dangermannn <dangermanplyt@gmail.com>
2021-04-12 11:56:07 +0200

    Fixed not working shopping-cart

--- ShoppingPlatform-SPA/src/app/_services/shopping-cart.service.ts
+++ ShoppingPlatform-SPA/src/app/_services/shopping-cart.service.ts
@@ -1,24 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { Product } from '../_models/product';
+import { AlertifyService } from './alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
-  products: Product[] = [];
-  
-  constructor() { }
+  products: Product[];
+
+  constructor(private alertify: AlertifyService) { }
+
 
   addProduct(product: Product){
     this.products = JSON.parse(localStorage.getItem('shopping-cart'));
-    if(this.products == null)
-      this.products = [];
     this.products.push(product);
-    localStorage.removeItem('shopping-cart');
-    localStorage.setItem('shopping-cart', JSON.stringify(this.products));
+    this.updateShoppingCart();
+  }
+
+  removeProduct(index: number){
+    this.alertify.message("INDEX: " + index);
+    this.products.splice(index, 1);
+    this.alertify.message("PRODUCTS SIZE IN SERVICE: " + this.products.length);
+    this.updateShoppingCart();
+    this.alertify.success('Product has ben removed from shopping-cart');
+  }
+
+  getProductIndex(product: Product){
+    for(let i = 0; i < this.products.length; i++)
+      if(JSON.stringify(product) === JSON.stringify(this.products[i]))
+        return i;
+    return -1;
   }
 
   getProducts(): Product[]{
-    return JSON.parse(localStorage.getItem('shopping-cart'));
+    this.products = JSON.parse(localStorage.getItem('shopping-cart'));
+    return this.products;
+  }
+
+  updateShoppingCart(){
+    localStorage.removeItem('shopping-cart');
+    localStorage.setItem('shopping-cart', JSON.stringify(this.products));
   }
 }
