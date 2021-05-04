bb3b7df82a5f2c76cb621077165e6a9322175ff0
Dangermannn <dangermanplyt@gmail.com>
2021-05-04 23:33:09 +0200

    SPA: Setting up for new transaction model

--- ShoppingPlatform-SPA/src/app/_models/transaction.ts
+++ ShoppingPlatform-SPA/src/app/_models/transaction.ts
@@ -4,7 +4,6 @@ import { User } from './user';
 export interface Transaction{
     id: number;
     initialized: Date;
-    seller: User;
     buyer: User;
     price: number;
     products: Product[];
