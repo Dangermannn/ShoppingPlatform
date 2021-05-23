2c8ab6293f2faa77c75d4d998a63cadc616440c6
Dangermannn <dangermanplyt@gmail.com>
2021-05-23 18:40:40 +0200

    SPA: Added removing product

--- ShoppingPlatform-SPA/src/app/user-products/user-products.component.ts
+++ ShoppingPlatform-SPA/src/app/user-products/user-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../_models/product';
 import { AlertifyService } from '../_services/alertify.service';
+import { ProductService } from '../_services/product.service';
 
 @Component({
   selector: 'app-user-products',
@@ -12,7 +13,8 @@ export class UserProductsComponent implements OnInit {
   myProducts: Product[];
   mySoldProducts: Product[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private productService: ProductService,
+    private alertify: AlertifyService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
@@ -23,4 +25,16 @@ export class UserProductsComponent implements OnInit {
     });
   }
 
+
+    removeProduct(product: Product): void{
+      this.productService.removeProduct(product.id).subscribe(data => {
+        const index = this.myProducts.indexOf(product);
+        if(index != -1){
+          this.myProducts.splice(index, 1);
+        }
+        this.alertify.success('Product has been removed');
+      }, error => {
+        this.alertify.error('Error occured while removing a product');
+      });
+    }
 }
